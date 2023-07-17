434acb5c3b351035a1a202148b2ff5b89bae3404
Ben Lloyd Pearson <ben.pearson@linearb.io>
2023-07-17 12:12:02 -0400

    Update javadoc.java

--- languages/java/javadoc/javadoc.java
+++ languages/java/javadoc/javadoc.java
@@ -86,4 +86,40 @@ public class RandomClass {
         int randomNumber = randomObj.generateRandomNumber(1, 10);
         System.out.println("Random number between 1 and 10: " + randomNumber);
     }
+        public static void main(String[] args) {
+        RandomClass randomObj = new RandomClass();
+        randomObj.sayHello();
+        int factorialResult = randomObj.factorial(5);
+        System.out.println("Factorial of 5: " + factorialResult);
+        String reversedString = randomObj.reverseString("Hello");
+        System.out.println("Reversed string: " + reversedString);
+        boolean isPrimeNumber = randomObj.isPrime(17);
+        System.out.println("Is 17 prime? " + isPrimeNumber);
+        int randomNumber = randomObj.generateRandomNumber(1, 10);
+        System.out.println("Random number between 1 and 10: " + randomNumber);
+    }
+        public static void main(String[] args) {
+        RandomClass randomObj = new RandomClass();
+        randomObj.sayHello();
+        int factorialResult = randomObj.factorial(5);
+        System.out.println("Factorial of 5: " + factorialResult);
+        String reversedString = randomObj.reverseString("Hello");
+        System.out.println("Reversed string: " + reversedString);
+        boolean isPrimeNumber = randomObj.isPrime(17);
+        System.out.println("Is 17 prime? " + isPrimeNumber);
+        int randomNumber = randomObj.generateRandomNumber(1, 10);
+        System.out.println("Random number between 1 and 10: " + randomNumber);
+    }
+        public static void main(String[] args) {
+        RandomClass randomObj = new RandomClass();
+        randomObj.sayHello();
+        int factorialResult = randomObj.factorial(5);
+        System.out.println("Factorial of 5: " + factorialResult);
+        String reversedString = randomObj.reverseString("Hello");
+        System.out.println("Reversed string: " + reversedString);
+        boolean isPrimeNumber = randomObj.isPrime(17);
+        System.out.println("Is 17 prime? " + isPrimeNumber);
+        int randomNumber = randomObj.generateRandomNumber(1, 10);
+        System.out.println("Random number between 1 and 10: " + randomNumber);
+    }
 }
