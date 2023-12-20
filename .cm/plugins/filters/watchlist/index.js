7916e0078f4d2b175e9f626b07e2e150b6228769
Ben Lloyd Pearson <ben.pearson@linearb.io>
2023-12-20 16:57:15 -0500

    Update index.js

--- .cm/plugins/filters/watchlist/index.js
+++ .cm/plugins/filters/watchlist/index.js
@@ -1,19 +1,15 @@
-
 const fs = require('fs');
-const yaml = require('js-yaml');
-
-const yamlFile = './watchlist.yml';
+const yaml = require('./watchlist.yml');
 
 try {
   // Read the YAML file
-  const yamlData = yaml.safeLoad(fs.readFileSync(yamlFile, 'utf8'));
+  const yamlData = yaml.safeLoad(fs.readFileSync(yaml, 'utf8'));
 
   // Now you can work with the parsed YAML data as a JavaScript object
   console.log('Parsed YAML data:', yamlData);
+  return yamlData;
 } catch (error) {
   console.error('Error reading or parsing YAML:', error);
 }
 
-if (yamlData) {
-    return yamlData;
-}
+
