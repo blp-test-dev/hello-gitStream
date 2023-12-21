42cbadd98b520fbae4405ca52f80650e1e84d992
Ben Lloyd Pearson <ben.pearson@linearb.io>
2023-12-21 15:56:27 -0500

    Update index.js

--- .cm/plugins/filters/gitstreamUsers/index.js
+++ .cm/plugins/filters/gitstreamUsers/index.js
@@ -1,5 +1,9 @@
 const gitstreamUsers = ["BenLloydPearson", "user2"];
 
+function containsString(arr, str) {
+    return arr.includes(str);
+}
+
 function users(username) {
     if (containsString(gitstreamUsers, username)) {
         return true;
