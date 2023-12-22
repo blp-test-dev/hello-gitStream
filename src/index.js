a9cd82390f377b9ac1e4bb21e1b16e2ef7acddf7
Ben Lloyd Pearson <ben.pearson@linearb.io>
2023-12-22 11:20:40 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,40 @@
+/**
+ * Generate a Markdown checklist indicating which new JavaScript files include specified JSDoc elements.
+ * @param {Array} fileMetadataList - List of file metadata objects.
+ * @returns {string} Markdown checklist.
+ */
+function generateMarkdownChecklist(fileMetadataList) {
+    const checklistItems = [];
+
+    fileMetadataList.forEach((fileMetadata) => {
+        const { new_file, new_content } = fileMetadata;
+
+      // Check if the file is a new JavaScript file
+        if (/\.js$/.test(new_file) && /^$/.test(fileMetadata.original_file)) {
+        // Split the new content into lines
+        const lines = new_content.split('\n');
+
+        // Check for the presence of specified JSDoc elements
+        const requiredTags = ['@description', '@module', '@returns', '@example', '@license'];
+        const missingTags = [];
+
+        requiredTags.forEach((tag) => {
+            const tagExists = lines.some((line) => line.includes(tag));
+            if (!tagExists) {
+            missingTags.push(tag);
+            }
+        });
+
+        // Generate checklist item
+        const checklistItem = `- [${new_file}](${new_file}) - ${
+            missingTags.length === 0 ? 'All required tags present' : 'Missing tags: ' + missingTags.join(', ')
+        }`;
+        checklistItems.push(checklistItem);
+        }
+    });
+
+    // Generate Markdown checklist
+    const markdownChecklist = checklistItems.join('\n');
+
+    return markdownChecklist;
+}
