4d46e26c06fcf4048e0a875635e969188f969a24
Ben Lloyd Pearson <ben.pearson@linearb.io>
2024-01-08 15:19:24 -0500

    Create index.js

---
+++ .cm/plugins/filters/extractMissingJSDocTags/index.js
@@ -0,0 +1,28 @@
+const requiredjsdocTags = ['@description', '@module', '@returns', '@param', '@example', '@license'];
+
+function extractMissingJsdocTags (fileMetadata) {
+    const response = [];
+
+    for (fileList in fileMetadata) {
+        console.log('Running JSDoc checklist for ' + fileList);
+        const modifiedFiles = fileMetadata[fileList];
+        const pluginIndex = modifiedFiles.find((item) => item.new_file.endsWith('index.js'));
+        const missingTags = [];
+
+        requiredjsdocTags.forEach((tag) => {
+            const tagExists = Boolean(pluginIndex.new_content.includes(tag));
+            if (!tagExists) {
+                missingTags.push(tag);
+            }
+        });
+        const output = {
+            file: pluginIndex.new_file,
+            missing_tags: missingTags,
+            source: pluginIndex.new_content
+        };
+        response.push(output);
+    }
+    return response;
+}
+
+module.exports = extractMissingJsdocTags;
