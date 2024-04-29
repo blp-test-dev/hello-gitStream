c0c5e6c4ef2e6a5a3cada21f418fb902748967d3
Ben Lloyd Pearson <ben.pearson@linearb.io>
2024-04-29 12:51:50 -0400

    Update index.js

--- .cm/plugins/filters/hasJiraTask/index.js
+++ .cm/plugins/filters/hasJiraTask/index.js
@@ -10,7 +10,7 @@
 module.exports = {
 	async: true,
 	filter: async (inputString, password, jiraSpaceName, email, callback) => {
-		const jql = `summmary = '${inputString}'`;
+		const jql = `summary = '${inputString}'`;
 		
     const resp = await fetch(`https://${jiraSpaceName}.atlassian.net/rest/api/2/search`, {
 			method: 'POST',
